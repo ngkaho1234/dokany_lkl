d05850b4432c48967bd5e5f265ee59d34c242056
Kaho Ng <ngkaho1234@gmail.com>
2016-03-03 21:59:36 +0800

    Initial commit.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-gcc -g -Iinclude -L. -Llkl -D_UNICODE -municode mirror.c utils.c -llkl -lws2_32 dokan1.lib dokannp1.lib  -o mirror.exe
+gcc -g -Iinclude -L. -Llkl -D_UNICODE -municode dokany_lkl.c utils.c -llkl -lws2_32 dokan1.lib dokannp1.lib  -o dokany_lkl.exe
