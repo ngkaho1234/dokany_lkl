55f96630311deb8c1551cc9f5efe17d5f936ffcd
Kaho Ng <ngkaho1234@gmail.com>
2016-03-05 21:01:24 +0800

    Changes 2.

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-gcc -g -Iinclude -L. -Llkl -D_UNICODE -municode dokany_lkl.c utils.c -llkl -lws2_32 dokan1.lib dokannp1.lib  -o dokany_lkl.exe
+#!/bin/sh
+${CC:=gcc} -g -Iinclude -Iinclude/lkl -L. -D_UNICODE -municode dokany-lkl.c utils.c -llkl -lws2_32 dokan1.lib dokannp1.lib  -o dokany-lkl.exe
